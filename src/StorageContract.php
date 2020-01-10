f8c114a53f2a3d8bac9e3bd61a4cbdcebc688b5c
ARandomInvestor <59703509+ARandomInvestor@users.noreply.github.com>
2020-01-10 14:26:20 +0100

    Added more methods to contract, added helper method to get lists

--- src/StorageContract.php
+++ src/StorageContract.php
@@ -27,7 +27,7 @@ class StorageContract {
     public function getFundPerformace(callable $return) {
         $this->contract->call("getAll", function ($err, $values) use ($return) {
             if ($err !== null) {
-                return null;
+                throw $err;
             }
             /** @var BigInteger[] $dates */
             /** @var BigInteger[] $performances */
@@ -48,11 +48,111 @@ class StorageContract {
     public function getAUM(callable $return) {
         $this->contract->call("aum", function ($err, $values) use ($return) {
             if ($err !== null) {
-                return null;
+                throw $err;
             }
             /** @var BigInteger[] $values */
             $return($values[0]->toString());
         });
+    }
+
+
+    /**
+     * NOTE: this method takes a long time. You might want to set a longer connection timeout on Web3 connection.
+     * @param callable $return
+     */
+    public function getInvestors(callable $return) {
+      $this->contract->call("getAllInvestors", function ($err, $values) use ($return) {
+          if ($err !== null) {
+              throw $err;
+          }
+          /** @var string[] $values */
+          $return($values);
+      });
+    }
+
+    private function getAllValues(callable $count, callable $getter, callable $result, ...$args) {
+      $count(...$args, ...[function($count) use ($getter, $result, $args){
+        $list = [];
+        if($count > 0){
+          for($n = 0; $n < $count; ++$n){
+            $getter(...$args, ...[$n, function($value) use (&$list, $result, $n, $count){
+              $list[] = $value;
+              if ($n === ($count - 1)) { //Done with all
+                $result($list);
+              }
+            }]);
+          }
+        }else{
+          $result($list);
+        }
+
+      }]);
+    }
+
+    /**
+     * @param callable $return
+     */
+    public function getDepositAddressCount(callable $return) {
+        $this->contract->call("fundDepositAddressesLength", function ($err, $values) use ($return) {
+            if ($err !== null) {
+                throw $err;
+            }
+
+            /** @var BigInteger[] $values */
+            $return($values[0]->toString());
+        });
+    }
+
+    /**
+     * @param int $n Address number to get
+     * @param callable $return
+     */
+    public function getDepositAddress($n, callable $return) {
+        $this->contract->call("fundDepositAddresses", $n, function ($err, $values) use ($return) {
+            if ($err !== null) {
+                throw $err;
+            }
+
+            /** @var string[] $values */
+            $return($values[0]);
+        });
+    }
+
+    public function getDepositAddresses(callable $return){
+      $this->getAllValues([$this, "getDepositAddressCount"], [$this, "getDepositAddress"], $return);
+    }
+
+    /**
+     * @param callable $return
+     */
+    public function getFeeAddressCount(callable $return) {
+        $this->contract->call("feeAddressesLength", function ($err, $values) use ($return) {
+            if ($err !== null) {
+                throw $err;
+            }
+
+            /** @var BigInteger[] $values */
+            $return($values[0]->toString());
+        });
+    }
+
+    /**
+     * @param int $n Address number to get
+     * @param callable $return
+     */
+    public function getFeeAddress($n, callable $return) {
+        $this->contract->call("feeAddresses", $n, function ($err, $values) use ($return) {
+            if ($err !== null) {
+                throw $err;
+            }
+
+            /** @var string[] $values */
+            $return($values[0]);
+        });
+    }
+
+    public function getFeeAddresses(callable $return){
+      $this->getAllValues([$this, "getFeeAddressCount"], [$this, "getFeeAddress"], $return);
     }
 
     /**
@@ -62,9 +162,10 @@ class StorageContract {
     public function getTxCount($address, callable $return) {
         $this->contract->call("ntx", $address, function ($err, $values) use ($return) {
             if ($err !== null) {
-                return null;
+                throw $err;
             }
 
+            /** @var BigInteger[] $values */
             $return($values[0]->toString());
         });
     }
@@ -77,10 +178,14 @@ class StorageContract {
     public function getTx($address, $n, callable $return) {
         $this->contract->call("getTx", $address, $n, function ($err, $values) use ($return) {
             if ($err !== null) {
-                return null;
+                throw $err;
             }
 
             $return(["txid" => $values[0], "pubkey" => $values[1], "address" => $values[2], "action" => $values[3]->toString(), "timestamp" => $values[4]->toString(),]);
         });
+    }
+
+    public function getTxs($address, callable $return){
+      $this->getAllValues([$this, "getTxCount"], [$this, "getTx"], $return, $address);
     }
 }
